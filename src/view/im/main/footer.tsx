6feb7a54cfccdb129b45dbbc3c1c81364829bc89
keyus <kisswen660@163.com>
2021-08-24 21:22:09 +0700

    message

--- src/view/im/main/footer.tsx
+++ src/view/im/main/footer.tsx
@@ -10,7 +10,28 @@ import { ReactComponent as IconText } from '@img/text.svg'
 import { ReactComponent as IconSend } from '@img/send.svg'
 
 const { Option } = Mentions;
+
+/**
+ * 使用 indexedDB 让历史聊天信息本地化,  不再去服务器读取历史记录
+ * 但仅支持当前使用电脑，本地持久化读取历史信息
+ */
 export default () => {
+
+    //发送信息
+    const sendMessage = useCallback(() => {
+        console.log('enter: handle something message send');
+
+    }, []);
+
+    const onKeyPress = useCallback((e) => {
+        // not shift + enter
+        if (e.which === 13 && !e.shiftKey) {
+            e.preventDefault();
+            sendMessage();
+        }
+    }, []);
+
+
 
     return (
         <div className='im-main-footer'>
@@ -26,6 +47,8 @@ export default () => {
                 <Mentions
                     className='im-mentions'
                     autoSize={{ minRows: 3 }}
+                    onKeyPress={onKeyPress}
+                    // onPressEnter={onPressEnter}
                     autoFocus
                     placement="top">
                     <Option value="afc163">afc163</Option>
@@ -34,7 +57,7 @@ export default () => {
                 </Mentions>
 
                 <div className='im-send'>
-                    <Tooltip title='按Enter发送信息，Ctrl+Enter换行' placement="left"><IconSend /></Tooltip>
+                    <Tooltip title='按Enter发送信息，Ctrl+Enter换行' placement="left"><IconSend onClick={sendMessage} /></Tooltip>
                 </div>
             </div>
         </div>
