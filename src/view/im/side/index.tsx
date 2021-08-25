785e6c8f63504b695df70b193e81738afb323128
keyus <kisswen660@163.com>
2021-08-25 16:56:34 +0700

    message

--- src/view/im/side/index.tsx
+++ src/view/im/side/index.tsx
@@ -27,24 +27,18 @@ export default () => {
                             </p>
                         </div>
                     </li>
-                    {
-                        new Array(15).fill(1).map((it, index) => {
-                            return (
-                                <li key={index}>
-                                    <div className='it-avatar'>
-                                        <Avatar2 />
-                                    </div>
-                                    <div className='it-content'>
-                                        <span className='it-user'>佛山无影大蒜</span>
-                                        <p>
-                                            <span>出来，有找要打你.</span>
-                                            <label>2021-08-22</label>
-                                        </p>
-                                    </div>
-                                </li>
-                            )
-                        })
-                    }
+                    <li>
+                        <div className='it-avatar'>
+                            <Avatar2 />
+                        </div>
+                        <div className='it-content'>
+                            <span className='it-user'>佛山无影大蒜</span>
+                            <p>
+                                <span>出来，有找要打你.</span>
+                                <label>2021-08-22</label>
+                            </p>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
